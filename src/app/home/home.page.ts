eb722fe0232cc729d14b741f801efe0997cbf576
André Ricardo da Costa <arc.andrecosta@gmail.com>
2018-11-25 20:15:49 -0200

    Adicionado array e componente de lista

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,50 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  tasks: any[] = [];
+
+  constructor(private alertCTRL: AlertController, private toastCTRL: ToastController) {
+
+  }
+  async showAdd() {
+    const alert = await this.alertCTRL.create({
+      header: 'O que deseja fazer?',
+      inputs: [
+        {
+          name: 'taskToDo',
+          type: 'text',
+          placeholder: 'Comprar pão'
+        }],
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'sencondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        },
+        {
+          text: 'Adicionar',
+          handler: (form) => {
+            this.add(form.taskToDo);
+          }
+        }]
+    });
+    await alert.present();
+  }
+  async add(taskToDo: string) {
+    if (taskToDo.trim().length < 1) {
+      const toast = await this.toastCTRL.create({
+        message: 'Informe o que deseja fazer!',
+        duration: 2000,
+        position: 'bottom'
+      });
+      toast.present();
+      return;
+    }
+    let task = { name: taskToDo, done: false };
+
+    this.tasks.push(task);
+  }
 }
